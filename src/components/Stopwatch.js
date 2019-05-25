c4fdcd0c06e8aa82122a9d11b77bd4e03a15d176
윤태영 <yh5636@naver.com>
2019-05-25 18:25:43 +0900

    Optimize

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -3,10 +3,6 @@ import React from 'react';
 export class Stopwatch extends React.Component {
   thickRef; //속성선언
 
-  componentWillUnmount() {
-    clearInterval(this.tickRef);
-  }
-
   state = { //시간은 변하는 값이니까 state로
     timer : 0,
     isRunning : false
@@ -25,24 +21,26 @@ export class Stopwatch extends React.Component {
 
   // ★Dom이 렌더링된 직후에 호출되는 라이프 사이클
   // 주로 네트워크 호출, 3rd 라이브러리 로딩 등의 역할을 수행
-    componentDidMount() {
+
+  componentDidMount() {
       this.thickRef = setInterval(this.thickRef, 1000);
 
   }
-
   // ★DOM이 파괴되기 직전에 호출되는 라이프 사이클
   // 리소스 해제 등의 역할을 수행
+
   componentWillUnmount(){
     clearInterval(this.thickRef);
   }
 
-  tick(){
+  tick = () => {
     if (this.state.isRunning){
       this.setState(prevState => ({
         timer: prevState.timer +1
       }))
     }
   }
+
   handleStopwatch = () => {
     this.setState(prevState => ({
       isRunning: !prevState.isRunning
